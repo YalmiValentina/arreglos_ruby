d38af8143df52a53b55c29dd9ee7eed5cae968bc
YalmiValentina <yalmivale@gmail.com>
2021-08-27 23:20:20 -0400

    te entrega el rango del array solicitado entre los ()

--- ruby/tarea1.rb
+++ ruby/tarea1.rb
@@ -1,16 +1,11 @@
 =begin
 
-
-
-
-
-.slice
 .shuffle
 .join
 .insert
 values_at() -> devuelve un arreglo con los valores específicados en los parametros. 
 e.g. a = %w{gato perro oso}; puts a.values_at(1,2).join(' and ') #=> "perro y oso"
-=end
+
 
 #primer metodo .at or .fetch
 a = [1,2,3,4,5]
@@ -27,8 +22,11 @@ puts a.reverse
 puts a.length
 
 #quinto metodo .sort
-puts a.sort
 
 b = ["a","d","b","c"]
 puts b.sort
 
+=end
+#sexto metodo .slice
+c = [ "a", "b", "c", "d", "e" ]
+puts c.slice(3,4)
